0b82ad98aef5d7872e632679d10985dacc732ca3
si2012si <44318985+si2012si@users.noreply.github.com>
2021-09-11 00:20:39 +0500

    Add files via upload

---
+++ figurisimeni/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+
+namespace figurisimeni
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   string Name;
+            int countsimv; // колличество символов
+            Sortirovka sortirovka = new Sortirovka(); // объявляю класс
+            Console.WriteLine("Введите что-нибудь");
+           Name = Console.ReadLine();
+            Name = "1"; 
+            Console.WriteLine("Сколько символов нужно?");
+            Console.WriteLine("Если вы хотите превратить в рисунок слово впишите 1");
+           countsimv =Convert.ToInt32(Console.ReadLine());
+            countsimv = 20; 
+           
+            int chois;
+            Console.WriteLine("Какой Узор предпочитаете?");
+            Console.WriteLine("1. Полупирамида");
+            Console.WriteLine("2. Пирамида");
+            Console.WriteLine("3. Ромб");
+            Console.WriteLine("4. Ромб Инверсия");
+            Console.WriteLine("5. ");
+            Console.WriteLine("6. ");
+            chois =Convert.ToInt32(Console.ReadLine()); 
+            Console.Clear();
+            
+            
+            
+            switch (chois)
+            
+            {
+                case 1:
+                    if (countsimv ==1)
+                        sortirovka.LeftPiramid(Name);
+                    else
+                        sortirovka.LeftPiramid(new string(Convert.ToChar(Name), countsimv));
+
+                    Console.ReadLine();
+                break;
+
+                case 2:
+                    if (countsimv == 1)
+                        sortirovka.Piramid(Name);
+                    else
+                        sortirovka.Piramid(new string(Convert.ToChar(Name), countsimv));
+                    Console.ReadLine();
+                    break;
+                case 3:
+                    if (countsimv == 1)
+                        sortirovka.Romb(Name);
+                    else
+                        sortirovka.Romb(new string(Convert.ToChar(Name), countsimv));
+                    break;
+                case 4:
+                    if (countsimv == 1)
+                        sortirovka.RombInvers(Name);
+                    else
+                        sortirovka.RombInvers(new string(Convert.ToChar(Name), countsimv));
+                        break;
+                default:
+                    sortirovka.LeftPiramid(Name);
+                    Console.ReadLine();
+                    break;
+            }
+            sortirovka.LeftPiramid(Name);
+        }
+    }
+}
---
+++ figurisimeni/Sortirovka.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace figurisimeni
+{
+    class Sortirovka
+    {
+        int count = 1;
+        int probel;
+        public void LeftPiramid(string Name)
+        {
+            int longN = Name.Length;
+            while (count < longN)
+            {
+                count++;
+                Console.WriteLine(Name.Substring(0, count));
+            }
+            return;
+        }
+        public void Piramid(string Name)
+        {
+            int longN = Name.Length;
+            {
+                probel = longN;
+
+                for (int i = 0; i < longN; i++)
+                {
+                    Console.WriteLine(new string('*', probel) + Name.Substring(0, count) + Name.Substring(0, count) + new string('*', probel));
+                    count++;
+                    probel--;
+                }
+            }
+            return;
+        }
+        public void Romb(string Name)
+        {
+            int longN = Name.Length;
+            {
+                probel = longN;
+
+                for (int i = 0; i <= longN; i++)
+                {
+                    Console.WriteLine(new string('*', probel) + Name.Substring(0, count) + Name.Substring(0, count) + new string('*', probel));
+                    count++;
+                    probel--;
+                    if (i == longN - 1)
+                    {
+                        count--;
+                        for (int j = 0; j < longN; j++)
+                        {
+                            Console.WriteLine(new string('*', probel) + Name.Substring(0, count) + Name.Substring(0, count) + new string('*', probel));
+                            count--;
+                            probel++;
+                        };
+                    }
+                }
+
+
+            }
+            return;
+        }
+        public void RombInvers(string Name)
+        {
+            int longN = Name.Length;
+            {
+                probel = 1;
+                count = longN-1;
+                for (int i = 1; i <= longN; i++)
+                {
+                    Console.WriteLine(Name.Substring(0, count) + new string('*', probel*2) + Name.Substring(0, count));
+                    count--;
+
+                    probel++;
+                    if (i == longN - 1)
+                    {
+                        
+                        for (int j = 0; j < longN; j++)
+                        {
+                            Console.WriteLine(Name.Substring(0, count) + new string('*', probel*2) + Name.Substring(0, count));
+                            count++;
+                            probel--;
+                        };
+                    }
+                }
+                return;
+            }
+
+
+        }
+    }
+}
